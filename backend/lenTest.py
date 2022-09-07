8e29b7ee38433e054b931b5c8539d24563494703
uuxxi X <2933171792@qq.com>
2022-09-07 17:28:06 +0800

    2022/09/07

---
+++ backend/lenTest.py
@@ -0,0 +1,3 @@
+s = '<div class="tit">截至9月6日24时新型冠状病毒肺炎疫情最新情况</div>'
+
+print(s[s.index('截至') + 2: s.index('日') + 1])
--- backend/writeIntoExcel.py
+++ backend/writeIntoExcel.py
@@ -1,18 +1,29 @@
 import random
 import time
-
+import pandas as pd
 import requests
 import re
+from openpyxl.workbook import Workbook
 
-today_is = ''
 urlInfo = []
-dataList = []
 fakeHeaders = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) \
     Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77',
     'Cookie': ''
 }  # 伪装浏览器用的请求头
+
+
+def write(cont, col1, col2, name):
+    key = list(cont.keys())
+    value = list(cont.values())
+
+    # 利用pandas模块先建立DateFrame类型，然后将两个上面的list存进去
+    result_excel = pd.DataFrame()
+    result_excel[col1] = key
+    result_excel[col2] = value
+    # 写入excel
+    result_excel.to_excel(name)
 
 
 def setCookie():
@@ -46,15 +57,10 @@ def getPageInfo(url):
     r.encoding = r.apparent_encoding
     temp = r.text
 
-    # 数据暂存
-    data = {}
-    proData1 = {}
-    proData2 = {}
-
     # 获取日期信息 <div class="tit">截至9月6日24时新型冠状病毒肺炎疫情最新情况</div>
     pattern = '<div class="tit">.+</div>'
     s = re.findall(pattern, temp)[0]
-    data['日期'] = s[s.index('截至') + 2: s.index('日') + 1]
+    today_is = s[s.index('截至') + 2: s.index('日') + 1]
 
     # 获取所有段落内容
     pattern = '<p style="text-align: justify; line-height: 1.5; text-indent: 2em; font-family: 仿宋,仿宋_GB2312; ' \
@@ -69,6 +75,9 @@ def getPageInfo(url):
     # 2.
     # 统计所有省份包括港澳台每日本土新增确诊人数及新增无症状感染人数，境外输入类型和疑似病例等无需统计。
 
+    # 数据暂存
+    data = {}
+
     # 新增确诊
     pattern = '本土病例<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">\d+</span>'
     s = re.findall(pattern, pInfo[0])[0]  # 第一个
@@ -79,56 +88,68 @@ def getPageInfo(url):
     s = re.findall(pattern, pInfo[4])[0]  # 第一个
     data["新增无症状"] = s[s.index('">') + 2:s.index('</span>')]
 
+    write(data, '新增类型', '新增数量', '中国大陆' + today_is + '疫情情况.xlsx')
+
     # (港澳台外)所有省份新增确诊
-    pattern = '本土病例<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]*</span>例（.+）'
+    data1 = {}
+    pattern = '本土病例<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]*</span>例（.+），'
     s1 = re.findall(pattern, pInfo[0])[0]
     s2 = re.findall('[\u4E00-\u9FA5]+<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]+</span>例', s1)
-    for i in range(1, len(s2) - 1):
-        proData1[s2[i][0:s2[i].index('<span')]] = s2[i][s2[i].index('\">') + 2: s2[i].index('</span>')]
-    data['各省新增确诊'] = proData1
+    for i in range(1, len(s2)):
+        data1[s2[i][0:s2[i].index('<span')]] = s2[i][s2[i].index('\">') + 2: s2[i].index('</span>')]
+    write(data1, '省份', '新增确诊数量', '各省份' + today_is + '新增确诊.xlsx')
+    print(data1)
 
     # (港澳台外)所有省份新增无症状
+    data2 = {}
     pattern = '本土<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]*</span>例（.+）'
     s1 = re.findall(pattern, pInfo[4])[0]
     s2 = re.findall('[\u4E00-\u9FA5]+<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]+</span>例', s1)
     for i in range(1, len(s2)):
-        proData2[s2[i][0:s2[i].index('<span')]] = s2[i][s2[i].index('\">') + 2: s2[i].index('</span>')]
-    data['各省新增无症状'] = proData2
+        data2[s2[i][0:s2[i].index('<span')]] = s2[i][s2[i].index('\">') + 2: s2[i].index('</span>')]
+    write(data2, '省份', '新增无症状数量', '各省份' + today_is + '新增无症状.xlsx')
 
     # 港澳台信息
     # 香港
+    data3 = {}
     pattern = '香港特别行政区<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]*</span>例'
     s = re.findall(pattern, pInfo[6])[0]
-    data["香港累计确诊"] = s[s.index('\">') + 2:s.index('</span>')]
+    data3["香港累计确诊"] = s[s.index('\">') + 2:s.index('</span>')]
     # 澳门
     pattern = '澳门特别行政区<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]+</span>例'
     s = re.findall(pattern, pInfo[6])[0]
-    data["澳门累计确诊"] = s[s.index('">') + 2:s.index('</span>')]
+    data3["澳门累计确诊"] = s[s.index('">') + 2:s.index('</span>')]
     # 台湾
     pattern = '台湾地区<span style="font-family: 仿宋,仿宋_GB2312; font-size: 16pt;">[0-9]+</span>例'
     s = re.findall(pattern, pInfo[6])[0]
-    data["台湾累计确诊"] = s[s.index('">') + 2:s.index('</span>')]
+    data3["台湾累计确诊"] = s[s.index('">') + 2:s.index('</span>')]
 
-    dataList.append(data)
+    write(data3, '省份', '疫情情况', '港澳台' + today_is + '疫情情况.xlsx')
 
 
 if __name__ == "__main__":
-    # 获取七天数据，不断尝试直到全部获取
-    cnt = -1
-    while cnt < 7:
-        # 设置Cookie
-        setCookie()
+    # cnt = 0
+    # # 获取URL
+    # while cnt < 1:
+    #     setCookie()
+    #     try:
+    #         getUrlInfo()
+    #         time.sleep(random.randint(0, 5))
+    #         cnt += 1
+    #     except:
+    #         print("false...")
+    #         time.sleep(random.randint(0, 5))
+    #
+    # cnt = 0
+    # # 获取当天数据
+    # while cnt < 1:
+    #     setCookie()
+    #     try:
+    #         getPageInfo(urlInfo[0])
+    #         time.sleep(random.randint(0, 5))
+    #         cnt += 1
+    #     except:
+    #         print("false...")
+    #         time.sleep(random.randint(0, 5))
 
-        try:
-            if cnt == -1:
-                getUrlInfo()
-                time.sleep(random.randint(0, 5))
-                cnt += 1
-            else:
-                getPageInfo(urlInfo[cnt])
-                time.sleep(random.randint(0, 5))
-                print(dataList)
-                cnt += 1
-        except:
-            print("false...")
-            time.sleep(random.randint(0, 5))
+    getPageInfo('http://www.nhc.gov.cn/xcs/yqtb/202209/b9867ea1be624141b41f461a431239d7.shtml')
